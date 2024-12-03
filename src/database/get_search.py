84842b7e4af78ef63b80f0e074961cd5a34c1220
hahahellooo <hahahello777@gmail.com>
2024-12-03 12:25:45 +0900

    search done need to add artist name

--- src/database/get_search.py
+++ src/database/get_search.py
@@ -3,12 +3,13 @@ from pydantic import BaseModel
 from motor.motor_asyncio import AsyncIOMotorClient
 from typing import Optional, List
 from bson import ObjectId
+from datetime import datetime
 
 # FastAPI 애플리케이션 객체 생성
 app = FastAPI()
 
 # MongoDB 연결을 위한 클라이언트
-client = AsyncIOMotorClient("mongodb+srv://hahahello777:NDWdS4f47d3uLnZ3@cluster0.5vlv3.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0")  # MongoDB 연결 URL (로컬 MongoDB)
+client = AsyncIOMotorClient("mongodb+srv://hahahello777:NDWdS4f47d3uLnZ3@cluster0.5vlv3.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0")  # MongoDB 연결 URL (로컬 MongoDB) 
 db = client['test']  # 데이터베이스 이름
 collection = db['test']  # 컬렉션 이름
 
@@ -21,6 +22,13 @@ class TicketData(BaseModel):
     end_date: Optional[str]
 
 
+# 날짜 문자열을 datetime 객체로 변환하는 함수
+def parse_date(date_string: str) -> Optional[datetime]:
+    try:
+        return datetime.strptime(date_string, "%Y.%m.%d").strftime("%Y.%m.%d")
+    except ValueError:
+        return None
+
 # API 엔드포인트 정의
 @app.get("/search", response_model=List[TicketData])
 async def search_tickets(
@@ -28,23 +36,33 @@ async def search_tickets(
     category: Optional[str] = Query(None, alias="category"),
     region: Optional[str] = Query(None, alias="region"),
     start_date: Optional[str] = Query(None, alias="start_date"),
+    end_date: Optional[str] = Query(None, alias="end_date"),
 ):
 
     # MongoDB 쿼리 조건 설정
     query = {}
-    
+
+    # 날짜 파싱
+    if start_date:
+        start_date = parse_date(start_date)
+        if start_date:
+            query["start_date"] = {"$lte": end_date}  
+
+    if end_date:
+        end_date = parse_date(end_date)
+        if end_date:
+            query["end_date"] = {"$gte": start_date}  
+
     if title:
         query["title"] = {"$regex": title, "$options": "i"}  # 대소문자 구분 없이 검색
     if category:
         query["category"] = {"$regex": category, "$options": "i"}
     if region:
         query["region"] = {"$regex": region, "$options": "i"}
-    if start_date:
-        query["start_date"] = {"$regex": start_date, "$options": "i"}
-    
+
     # MongoDB에서 쿼리 실행
     cursor = collection.find(query)
-    
+
     # 결과 반환
     tickets = []
     async for ticket in cursor:
@@ -56,5 +74,6 @@ async def search_tickets(
             "end_date": ticket.get("end_date"),
         }
         tickets.append(ticket_data)
-    
+
     return tickets
+
