6e6b17e1ceb47ce16241a59b452a9b3ceed389d3
hahahellooo <hahahello777@gmail.com>
2024-12-12 17:45:14 +0900

    add CORS

--- src/database/main.py
+++ src/database/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from src.database.routers import tickets, banner, weekend, rank  # tickets 라우터를 포함한 모듈
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000/"],
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=[""],
+)
 
 # 라우터를 메인 앱에 연결
 app.include_router(tickets.router)
