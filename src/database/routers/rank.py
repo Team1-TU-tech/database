90837193e3cde11da96c11960111a6d0ec91c0af
hahahellooo <hahahello777@gmail.com>
2024-12-18 16:50:09 +0900

    fix list_objects_v2

--- src/database/routers/rank.py
+++ src/database/routers/rank.py
@@ -34,21 +34,23 @@ s3_client = boto3.client('s3',
 
 def get_logs(bucket_name: str = "t1-tu-data", directory: str = 'view_detail_log/') -> List[dict]:
     try:
-        # S3에서 디렉토리 내 파일 목록 가져오기
-        response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=directory)
+        # Paginator 설정
+        paginator = s3_client.get_paginator('list_objects_v2')
+        operation_parameters = {'Bucket': bucket_name, 'Prefix': directory}
 
-        # 파일 목록을 가져왔는지 확인
-        if 'Contents' not in response:
-            print("No files found in the specified directory.")
-            return []
+        # 전체 Parquet 파일 키를 저장할 리스트
+        parquet_files = []
 
-        # Parquet 파일만 필터링
-        parquet_files = [obj['Key'] for obj in response['Contents'] if obj['Key'].endswith('.parquet')]
+        # Paginator를 사용하여 여러 페이지에서 파일 키 가져오기
+        for page in paginator.paginate(**operation_parameters):
+            if 'Contents' in page:
+                # Parquet 파일만 필터링
+                page_files = [obj['Key'] for obj in page['Contents'] if obj['Key'].endswith('.parquet')]
+                parquet_files.extend(page_files)
 
         if not parquet_files:
             print("No Parquet files found.")
             return []
-
         # Parquet 파일을 읽어 pandas DataFrame으로 결합
         logs = []
         for file_key in parquet_files:
